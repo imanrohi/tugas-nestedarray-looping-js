09ab36af17aa6ab60f5436ff9be8a2d3c5b53f3e
User-niomic <user@niomic.com>
2021-08-08 01:17:23 +0700

    TUGAS NESTED ARRAY LOOPING JAVASCRIPT
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas-NestedArray-Looping.js
@@ -0,0 +1,13 @@
+function panggilNestedArray(value) {
+  for (var i = 0; i <= value.length; i++) {
+    console.log(value[0][i],value[1][i],value[2][i]);
+  }
+
+}
+var nestedArray = [
+   [1,2,3,4],
+   ['Mark Zuckerberg', 'Elon Musk', 'Bill Gates', 'Steve Jobs'],
+   ['Facebook', 'Tesla', 'Microsoft', 'Apple']
+]
+
+panggilNestedArray(nestedArray)
